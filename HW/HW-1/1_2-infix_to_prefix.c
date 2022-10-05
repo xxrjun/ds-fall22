4623dd73a569f631b4ba00f55841deb9e177205e
xxrjun <rjun0729@gmail.com>
2022-10-05 10:45:48 +0800

    add hw1-2.

---
+++ HW/HW-1/1_2-infix_to_prefix.c
@@ -0,0 +1,15 @@
+/**
+ * @file 1_2-infix_to_prefix.c
+ * @author your name (you@domain.com)
+ * @brief NCU DS fall 2022 HW1_2 infix to prefix
+ * @date 2022-10-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+}
